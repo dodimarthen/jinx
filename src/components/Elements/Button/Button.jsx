8e61ac4ecf10f61468e6e0757b0c439367ffbb81
dodimarthen <dodimartin.sit@gmail.com>
2024-07-29 21:38:36 +0700

    create form login

--- src/components/Elements/Button/Button.jsx
+++ src/components/Elements/Button/Button.jsx
@@ -1,10 +1,11 @@
 const Button = (props) => {
+  const { children, classname = "bg-blue-400" } = props;
   return (
     <button
-      className={`h-10 px-6 font-semibold rounded-md ${props.variant} text-white`}
+      className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`}
       type="submit"
     >
-      {props.text}
+      {children}
     </button>
   );
 };
