052d4773b026fcbe1bf2778b28223af849cfe9d0
dodimarthen <dodimartin.sit@gmail.com>
2024-07-30 14:41:18 +0700

    make adjustment

--- src/components/Layouts/AuthLayout.jsx
+++ src/components/Layouts/AuthLayout.jsx
@@ -1,12 +1,14 @@
 const AuthLayout = (props) => {
   const { children, title } = props;
   return (
-    <div className="w-full max-w-xs">
-      <h1 className="text-4xl font-bold mb-2 text-blue-700">{title}</h1>
-      <p className="font-medium text-slate-600 mb-8">
-        Welcome, please fill the details below!
-      </p>
-      {children}
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="w-full max-w-xs">
+        <h1 className="text-4xl font-bold mb-2 text-blue-700">{title}</h1>
+        <p className="font-medium text-slate-600 mb-8">
+          Welcome, please fill the details below!
+        </p>
+        {children}
+      </div>
     </div>
   );
 };
